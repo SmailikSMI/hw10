b9198567b11e9b5376099e3a35e94c009b6640d3
SmailikSMI <smailiksmi882@gmail.com>
2023-02-12 17:56:38 +0300

    Исправленны задания с паспортом,номером карты,

--- HomeWork/src/by/teachmeskills/siniak/homework10/util/StringUtils/StringUtils.java
+++ HomeWork/src/by/teachmeskills/siniak/homework10/util/StringUtils/StringUtils.java
@@ -9,15 +9,18 @@ public class StringUtils {
     public static String removeSpace(String string) {
         string = string.trim();
         for (int i = 0; i < string.length(); i++) {
-            string = string.replace("  ", " ");
+            string = string.replace(" ", "");
         }
         return string;
     }
 
+
+    public static final int CARD_NUMBER_LENGTH = 16;
+    public static final int MASCED_CARD_NUMBER_LENGTH = 14;
     public static String hideNumBankCard(String string) {
-        if (string.length() == 16) {
-            String subString = string.substring(0, 12);
-            string = string.replace(subString, "************");
+        if (string.length() == CARD_NUMBER_LENGTH) {
+            String hidedenCardNumber = string.substring(CARD_NUMBER_LENGTH-MASCED_CARD_NUMBER_LENGTH);
+            string = string.replace(hidedenCardNumber, "************");
         }
         return string;
     }
@@ -25,7 +28,7 @@ public class StringUtils {
     public static String shortenNames(String SecondName, String FirstName, String MiddleName) {
         FirstName = FirstName.charAt(0) + ".";
         if (!MiddleName.isEmpty()) {
-            MiddleName = MiddleName.charAt(0) + ".";
+            MiddleName = MiddleName.charAt(0) + ". ";
         }
         return SecondName + FirstName + MiddleName;
     }
@@ -38,19 +41,42 @@ public class StringUtils {
         return string;
     }
 
-    public static boolean checkNumPassport(String string) {
+    public static boolean isUpperWords(String string){
         boolean isUpperWord1 = Character.isUpperCase(string.charAt(0));
         boolean isUpperWord2 = Character.isUpperCase(string.charAt(1));
-        if (!(isUpperWord1 && isUpperWord2)) {
-            return false;
-        }
+        return isUpperWord1 && isUpperWord2;
+    }
+
+    public static boolean isEnglishWords(String string){
         boolean isEnglishWord1 = (string.charAt(0) >= 'A' && string.charAt(0) <= 'Z');
         boolean isEnglishWord2 = (string.charAt(1) >= 'A' && string.charAt(1) <= 'Z');
-        if (!(isEnglishWord1 && isEnglishWord2)) {
+        return isEnglishWord1 && isEnglishWord2;
+    }
+
+    public static final int PASPORT_SERIES_LENGTH=2;
+
+    public static boolean isArabianDigits(String string){
+        for (int i = PASPORT_SERIES_LENGTH; i < string.length(); i++) {
+            if (!('0'>=string.charAt(i) && string.length()<='9')) {
+                return false;
+            }
+        }
+        return true;
+    }
+    public static final int PASSPORT_NUMBERS_LENGTH=7;
+
+    public static boolean checkNumPassport(String string) {
+        if (!StringUtils.isUpperWords(string)) {
             return false;
         }
-        for (int i = 2; i < string.length(); i++) {
-            if (!(Character.isDigit(string.charAt(i)) && string.length() - 1 == 8)) {
+
+        if (!isEnglishWords(string)) {
+            return false;
+        }
+
+        for (int i = PASPORT_SERIES_LENGTH; i < string.length(); i++) {
+            if (!(StringUtils.isArabianDigits(string) &&
+                    string.length() == PASPORT_SERIES_LENGTH + PASSPORT_NUMBERS_LENGTH)) {
                 return false;
             }
         }
